eb03b59f60b4c5bcb46a16d7ba1bd249b6b6e643
Wang1 <Wenbo.Wang1@kuka.com>
2020-05-09 13:39:41 +0800

    jump to method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isCollapsed = false;
+	isCollapsed = false;
+	constructor(private router: Router, private route: ActivatedRoute) { }
+	public jumpTo(): void {
+		// this.router.navigate(['/'], { fragment: '#/client-a/page1' });
+		window.location.href = `http://localhost:4200/#/client-a/page1`
+	}
 }
