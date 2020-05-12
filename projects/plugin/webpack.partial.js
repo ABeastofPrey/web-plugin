d734aa1431c740589d11ee7449d7ee889492014f
Wang1 <Wenbo.Wang1@kuka.com>
2020-05-12 16:17:33 +0800

    add ZipPlugin

--- projects/plugin/webpack.partial.js
+++ projects/plugin/webpack.partial.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack');
- 
+const ZipPlugin = require('zip-webpack-plugin');
 module.exports = {
+    output: {
+        filename: '[name].js',
+    },
     plugins: [
         new webpack.DefinePlugin({
             "VERSION": JSON.stringify("4711")
+        }),
+        new ZipPlugin({
+            filename: 'plugin.zip'
         })
     ]
 }
