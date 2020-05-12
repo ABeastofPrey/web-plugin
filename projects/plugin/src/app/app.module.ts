0c2e7ccc6b235127e6dbd53ae569b923d4290449
Wang1 <Wenbo.Wang1@kuka.com>
2020-05-12 14:50:23 +0800

    build plugin

--- projects/plugin/src/app/app.module.ts
+++ projects/plugin/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,6 +13,15 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const appEle = createCustomElement(AppComponent, { injector: this.injector });
+    console.log('Define custom plugin element');
+    customElements.define('my-plugin', appEle);
+  }
+}
